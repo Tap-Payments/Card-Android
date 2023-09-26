0f3a9c499694a8f73c5ca80ba70d33b6bf3a28e2
Aslm <130293799+aslm-android@users.noreply.github.com>
2023-09-26 06:32:10 -0300

    added enum for channel status

--- tapcardformkit/src/main/java/company/tap/tapcardformkit/open/web_wrapper/enums/enums.kt
+++ tapcardformkit/src/main/java/company/tap/tapcardformkit/open/web_wrapper/enums/enums.kt
@@ -5,3 +5,6 @@ package company.tap.tapcardformkit.open.web_wrapper.enums
 enum class CardFormWebStatus() {
     onReady, onFocus, onValidInput, onInvalidInput, onError, onSuccess, onBinIdentification, onHeightChange, on3dsRedirect,onScannerClick,onNfcClick
 }
+enum class PaymentChannels{
+    PAYMENT_TRANSACTION,RECURRING_TRANSACTION,INSTALLMENT_TRANSACTION,ADD_CARD,CARDHOLDER_VERIFICATION
+}
