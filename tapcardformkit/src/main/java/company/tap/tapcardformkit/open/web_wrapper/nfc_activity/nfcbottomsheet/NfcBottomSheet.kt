9c79fc14274ba376378dcc021e2712deac1ea2b0
Aslm <130293799+aslm-android@users.noreply.github.com>
2023-10-19 05:23:33 -0300

    loadlottie is called in oncreate

--- tapcardformkit/src/main/java/company/tap/tapcardformkit/open/web_wrapper/nfc_activity/nfcbottomsheet/NfcBottomSheet.kt
+++ tapcardformkit/src/main/java/company/tap/tapcardformkit/open/web_wrapper/nfc_activity/nfcbottomsheet/NfcBottomSheet.kt
@@ -1,27 +1,26 @@
 package company.tap.tapcardformkit.open.web_wrapper.nfc_activity.nfcbottomsheet
 
+import TapLocal
+import TapTheme
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
-import androidx.fragment.app.FragmentTransaction
 import com.airbnb.lottie.LottieAnimationView
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import company.tap.tapcardformkit.*
+import company.tap.tapcardformkit.R
+import company.tap.tapcardformkit.doAfterSpecificTime
+import company.tap.tapcardformkit.getDeviceLocale
+import company.tap.tapcardformkit.getDeviceTheme
 import company.tap.tapcardformkit.open.DataConfiguration
-import company.tap.tapcardformkit.open.web_wrapper.packageName
-import company.tap.tapcardformkit.open.web_wrapper.rawFolderRefrence
-import company.tap.tapuilibrary.uikit.fragment.NFCFragment
 import company.tap.tapuilibrary.uikit.views.TapBrandView
 
 class NfcBottomSheet : BottomSheetDialogFragment() {
 
-    private val mShimmerViewContainer by lazy { view?.findViewById<LottieAnimationView>(R.id.shimmer_view)}
-
+    private lateinit var  mShimmerViewContainer :LottieAnimationView
 
     @Nullable
     override fun onCreateView(
@@ -29,9 +28,12 @@ class NfcBottomSheet : BottomSheetDialogFragment() {
         @Nullable savedInstanceState: Bundle?
     ): View? {
         val view = LayoutInflater.from(context).inflate(R.layout.nfc_bottom_sheet, null)
+        mShimmerViewContainer =  view.findViewById<LottieAnimationView>(R.id.shimmer_view)
+        loadLottie()
         return view
     }
-    fun loadLottie(){
+
+    fun loadLottie() {
         val tapInterface = DataConfiguration.configurationsAsHashMap?.get("interface") as? Map<*, *>
         val lanugage = tapInterface?.get("locale")?.toString() ?: getDeviceLocale()?.language
         val theme = tapInterface?.get("theme")?.toString() ?: requireContext().getDeviceTheme()
@@ -39,7 +41,9 @@ class NfcBottomSheet : BottomSheetDialogFragment() {
             TapLocal.en.name->{
                 when(theme){
                     TapTheme.light.name ->setAnimationUrl("https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_light_en.json")
-                    TapTheme.dark.name->setAnimationUrl("https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_dark_en.json")
+                    TapTheme.dark.name->setAnimationUrl(
+                        "https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_dark_en.json"
+                    )
                     else -> {
 
                     }
@@ -47,30 +51,25 @@ class NfcBottomSheet : BottomSheetDialogFragment() {
             }
             TapLocal.ar.name->{
                 when(theme){
-                    TapTheme.light.name ->setAnimationUrl("https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_light_ar.json")
-                    TapTheme.dark.name->setAnimationUrl("https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_dark_ar.json")
+                    TapTheme.light.name ->setAnimationUrl(
+                        "https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_light_ar.json"
+                    )
+                    TapTheme.dark.name->setAnimationUrl(
+                        "https://tap-assets.b-cdn.net/card-sdk/nfc/nfc_dark_ar.json"
+                    )
                     else -> {}
                 }
             }
         }
 
     }
-    fun setAnimationUrl(url:String){
-        mShimmerViewContainer?.setAnimationFromUrl(url)
+    fun setAnimationUrl(url: String){
+        mShimmerViewContainer.setAnimationFromUrl(url)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val tapBrandView = view.findViewById<TapBrandView>(R.id.tab_brand_view_nfc)
-
-        loadLottie()
-
-//        val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
-//        transaction.replace(R.id.nfc_view, NFCFragment())
-//        transaction.addToBackStack(null)
-//        transaction.commit()
-
-
 
         tapBrandView.backButtonLinearLayout.setOnClickListener {
             this.dismiss()
@@ -78,16 +77,26 @@ class NfcBottomSheet : BottomSheetDialogFragment() {
 
         }
         this.dialog?.setOnDismissListener {
-            doAfterSpecificTime {
+            doAfterSpecificTime(time = 500) {
                 requireActivity().finish()
             }
         }
 
+
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         this.dialog?.window?.attributes?.windowAnimations = R.style.DialogAnimations
+
         setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialogFragment)
+
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        return super.onCreateDialog(savedInstanceState)
+
 
     }
 
